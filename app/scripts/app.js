30bc26c19d85a48d85129abd719bce0f52caffa0
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-11-10 11:51:54 -0300

    partial commit of header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,19 @@
 require('../stylesheets/app.scss');
 require('bootstrap/dist/css/bootstrap.css')
-
-
 import { Router, Route, Link } from 'react-router'
 import React from 'react';
 import { render } from 'react-dom';
-import {Map} from './components/map.jsx'
+import {Geocoding} from './components/geocoding.jsx'
+
+import {HeaderNavBar}  from './components/headerNavBar.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <p>
-         Landing Page of Geocoding tool.
-        </p>
-        <ol>
-          <li><Link to="/list" activeClassName="active">Pick a project</Link></li>
-          <li><Link to="/map" activeClassName="active">Open Empty Map</Link></li>
-          <li><Link to="/forgot-password" activeClassName="active">Leave the page</Link></li>
-        </ol>
-        {this.props.children}
+      Header
+          <HeaderNavBar/>
+          {this.props.children}
       </div>
     )
   }
@@ -35,7 +29,7 @@ class NoMatch extends React.Component{
 render((
   <Router>
     <Route path="/" component={App}>
-      <Route path="map" component={Map}/>
+      <Route path="geocoding" component={Geocoding}/>
        <Route path="*" component={NoMatch}/>
     </Route>
   </Router>
